c3f657b2a382672a6e454d82a5b6831407b51e17
Weronika Utrata <werutr@wp.pl>
2022-04-03 19:59:22 +0200

    js function added

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,26 @@
-console.log("Witam wszystkich!");
-
-let button = document.querySelector(".button");
-let site = document.querySelector(".site");
-let themeName = document.querySelector(".themeName");
-
-button.addEventListener("click", () => {
-  site.classList.toggle("blueTheme");
-  if (site.classList.contains("blueTheme")) {
-    themeName.innerText = "jasny";
-  } else {
-    themeName.innerText = "błękitny";
+{
+  const welcome = () => {
+    console.log("Witam wszystkich!");
   }
-});
+
+  const onChangeBackgroundClick = () => {
+    const site = document.querySelector(".site");
+    const themeName = document.querySelector(".themeName");
+
+    site.classList.toggle("blueTheme");
+    if (site.classList.contains("blueTheme")) {
+      themeName.innerText = "jasny";
+    } else {
+      themeName.innerText = "błękitny";
+    }
+  };
+
+  const init = () => {
+    const button = document.querySelector(".button");
+    button.addEventListener("click", onChangeBackgroundClick);
+
+    welcome();
+  };
+
+  init();
+}
